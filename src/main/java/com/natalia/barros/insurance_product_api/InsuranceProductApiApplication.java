3262b802911217f3ed5a76230103a1930f090a46
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2026-03-10 11:40:23 -0300

    initial commit

---
+++ src/main/java/com/natalia/barros/insurance_product_api/InsuranceProductApiApplication.java
@@ -0,0 +1,13 @@
+package com.natalia.barros.insurance_product_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceProductApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceProductApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natalia/barros/insurance_product_api/InsuranceProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natalia.barros.insurance_product_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
