a3a29131a153bd5b669a648d4a6bd6aaba383ac2
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2026-03-10 14:44:11 -0300

    implementando tratamento de erros

--- src/main/java/com/natalia/barros/insurance_product_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/natalia/barros/insurance_product_api/exception/GlobalExceptionHandler.java
@@ -1,4 +1,25 @@
 package com.natalia.barros.insurance_product_api.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import  org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidation(
+            MethodArgumentNotValidException ex) {
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
+
+        return ResponseEntity.badRequest().body(errors);
+    }
 }
