966522bfab2978aa15ca638153069cea5da29970
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2026-03-12 18:33:25 -0300

    refactor: refatorei codigo, removi switch para map

--- src/main/java/com/natalia/barros/insurance_product_api/factory/TaxStrategyFactory.java
+++ src/main/java/com/natalia/barros/insurance_product_api/factory/TaxStrategyFactory.java
@@ -4,17 +4,23 @@ import com.natalia.barros.insurance_product_api.domain.Category;
 import com.natalia.barros.insurance_product_api.strategy.*;
 import org.springframework.stereotype.Component;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 @Component
 public class TaxStrategyFactory {
+
+    private final Map<Category, TaxStrategy> strategies = new EnumMap<>(Category.class);
+
+    public TaxStrategyFactory() {
+        strategies.put(Category.VIDA, new VidaTaxStrategy());
+        strategies.put(Category.AUTO, new AutoTaxStrategy());
+        strategies.put(Category.VIAGEM, new ViagemTaxStrategy());
+        strategies.put(Category.RESIDENCIAL, new ResidencialTaxStrategy());
+        strategies.put(Category.PATRIMONIAL, new PatrimonialTaxStrategy());
+    }
+
     public TaxStrategy getStrategy(Category category) {
-
-        return switch (category) {
-
-            case VIDA -> new VidaTaxStrategy();
-            case AUTO -> new AutoTaxStrategy();
-            case VIAGEM -> new ViagemTaxStrategy();
-            case RESIDENCIAL -> new ResidencialTaxStrategy();
-            case PATRIMONIAL -> new PatrimonialTaxStrategy();
-        };
+        return strategies.get(category);
     }
 }
